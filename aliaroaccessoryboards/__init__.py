7ad45ca9dc85786dd05924429e4dde07296ca14a
Tanner Blair <tanner.blair@aliaro.com>
2025-02-13 17:00:18 -0600

    Add exception classes to module exports
    This update includes `PathUnsupportedException`, `ResourceInUseException`, and `SourceConflictException` in the module exports. These changes ensure the exceptions are accessible directly from the module, improving usability and consistency.

--- aliaroaccessoryboards/__init__.py
+++ aliaroaccessoryboards/__init__.py
@@ -5,10 +5,13 @@ __all__ = [
     'Topology',
     'BoardController',
     'I2CDriverBoardController',
+    'PathUnsupportedException',
+    'ResourceInUseException',
+    'SourceConflictException'
 ]
 
 from aliaroaccessoryboards.boardcontrollers.board_controller import BoardController
 from aliaroaccessoryboards.boardcontrollers.i2cdriver_board_controller import I2CDriverBoardController
 from aliaroaccessoryboards.switch_config import Topology
 
-from aliaroaccessoryboards.accessory_board import AccessoryBoard
+from aliaroaccessoryboards.accessory_board import AccessoryBoard, PathUnsupportedException, ResourceInUseException, SourceConflictException
